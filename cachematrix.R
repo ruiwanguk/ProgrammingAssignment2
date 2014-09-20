71b0c86ab1a2cbc19e2806cd0c0d11694a84cf2a
Rui Wang <harry.r.wang@gmail.com>
2014-09-20 19:52:43 +0100

    add tested business logic

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	matrixInverse <- NULL
+	
+	set <- function(y) {
+		x <<- y
+		matrixInverse <<- NULL
+	}
+	
+	get <- function() x
+	
+	setInverse <- function(inverse) matrixInverse <<- inverse
+	
+	getInverse <- function() matrixInverse
+	
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
+	matrixInverse <- x$getInverse()
+	
+	if (!is.null(matrixInverse)) {
+		message("Retrieved cached data")
+		return(matrixInverse)
+	}        
+	
+	matrixData <- x$get()
+	matrixInverse <- solve(matrixData, ...)
+	x$setInverse(matrixInverse)
+	matrixInverse
 }
